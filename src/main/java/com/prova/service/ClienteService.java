c053bc388faefb1db2bc6561270af45fe480b1cc
Haylson Martins <haylson@gmail.com>
2017-06-21 14:54:47 -0300

    commit inicial

---
+++ src/main/java/com/prova/service/ClienteService.java
@@ -0,0 +1,37 @@
+package com.prova.service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.prova.model.Cliente;
+import com.prova.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	ClienteRepository clienteRepository; 
+	
+	public Cliente cadastrar(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+
+	public Collection<Cliente> buscarTodos() {
+		return clienteRepository.findAll();
+	}
+	
+	public void excluir (Cliente cliente){
+		clienteRepository.delete(cliente);
+	}
+	
+	public Cliente buscarPorId(Integer id) {
+		return clienteRepository.findOne(id);
+	}
+	
+	/*public Cliente alterar(Cliente cliente){
+		return clienteRepository.save(cliente);
+	}*/
+
+}
---
+++ src/test/java/com/prova/ClienteApplicationTests.java
@@ -0,0 +1,18 @@
+package com.prova;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ClienteApplication.class)
+@WebAppConfiguration
+public class ClienteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
